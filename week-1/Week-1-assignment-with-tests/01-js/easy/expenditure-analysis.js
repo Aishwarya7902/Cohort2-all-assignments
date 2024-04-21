3d9996f537e5ca53cfd92844f6d4359894d09695
AISHWARYA GUPTA <71867316+Aishwarya7902@users.noreply.github.com>
2024-04-21 01:56:49 -0700

    Update expenditure-analysis.js

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -9,7 +9,22 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const totals={};
+  transactions.forEach((transaction)=>{
+   const {category,price} =transaction;
+   if(!totals[category]){
+    totals[category]=price;
+   }
+   else{
+    totals[category]+=price;
+   }
+  });
+
+  const result=[];
+  Object.keys(totals).forEach((category)=>{
+     result.push({category:category,totalSpent:totals[category]});
+  });
+  return result;
 }
 
 module.exports = calculateTotalSpentByCategory;
